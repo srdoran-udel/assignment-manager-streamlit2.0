3cefdb978b631fa108dd441d45a0d56a8fb4e899
Sophia Doran <srdoran@udel.edu>
2026-02-25 12:08:43 -0500

    test cause streamlit isnt working...

--- app_day2.py
+++ app_day2.py
@@ -2,4 +2,31 @@ import streamlit as st
 
 st.title("Course Management App")
 st.header("Assignment Management")
+st.markdown("-------------")
 st.subheader("Dashboard")
+
+st.divider()
+st.markdown("")
+
+#load data
+assignments = [{"id":"HW1",
+                "title":"Intro to Database",
+                "description":"basics of database design",
+                "points":100,
+                "type":"homework"}
+                ,
+                {"ID":"HW2",
+                "title":"Normalization",
+                "description":"normalizing",
+                "points":100,
+                "type":"homework"}]
+
+#input 
+#st.markdown("# Add New Assignment")
+st.markdown("## Add New Assignment")
+#st.markdown("### Add New Assignment")
+
+title = st.text_input("Title")
+description = st.text_area("Description",placeholder="Normalization is covered here",help="Here you are entering the assignment details")
+points = st.number_input("Points")
+
