ff66aba9bbb8a2a1f96971b6080db144df812697
Sophia Doran <srdoran@udel.edu>
2026-02-25 12:25:30 -0500

    todays work 2/25/25

--- app_day2.py
+++ app_day2.py
@@ -30,3 +30,20 @@ title = st.text_input("Title")
 description = st.text_area("Description",placeholder="Normalization is covered here",help="Here you are entering the assignment details")
 points = st.number_input("Points")
 
+#assignment_type = st.text_input("Assignment Type")
+assignment_type = st.radio("Type", ["Homework","Lab"],horizontal=True)
+st.caption ("Homework Type")
+assignment_type2 = st.selectbox("Type", ["Select an option","Homeork","Lab","Other"])
+
+if assignment_type2 == "other":
+    assignment_type2 = st.text_input("Type", placeholder="Enter the assignment type")
+
+due_date = st.date_input("Due_Date")
+
+#button when changing between pages, next page action 
+btn_save = st.button("Save", width="stretch")
+if btn_save:
+    st.warning("Working on it...")
+
+
+
