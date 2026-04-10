00e36fe4edb6e7a9174d8256218dba69be4b652e
Sophia Doran <srdoran@udel.edu>
2026-04-10 12:25:26 -0400

    4/10

--- app_module5_refactored.py
+++ app_module5_refactored.py
@@ -20,15 +20,23 @@ def save_data(assignments, json_path):
 
 #service layer
 
-def add_new_assignment(assignments, title, description, points, type):
+def add_new_assignment(assignments):
      assignments.append({"id": str(uuid.uuid4()),
-                                "title": title,
-                                "description": description,
-                                "points": points,
-                                "type": type})
+                                "title": st.session_state['draft']['title'],
+                                "description": st.session_state['draft']['description'],
+                                "points": st.session_state['draft']['points'],
+                                "type": st.session_state['draft']['assignment_type']})
      return assignments
 
-
+def edit_assignment(assignments: list) -> list:
+    for assignment in assignments:
+        if assignment["id"] == st.session_state["draft"]["id"]:
+            assignments['title'] = st.session_state["draft"]['title']
+            assignments['description'] = st.session_state["draft"]['description']
+            assignments['points'] = st.session_state["draft"]['points']
+            assignments['type'] = st.session_state["draft"]['assignment_type']
+            break
+    return assignments
 
 # UI layer 
 
@@ -40,67 +48,68 @@ def render_dashboard(assignments, json_path):
         if st.button("Back", key="back_btn", use_container_width=False):
             st.session_state["page"] = "Assignments Dashboard" 
             st.rerun()
-
-    # Form Inputs
-    st.session_state['draft']['title'] = st.text_input("Title")
-    st.session_state['draft']['description'] = st.text_area("Description", placeholder="normalization is covered here",
-                            help="Here you are entering the assignment details")
-    st.session_state["draft"]['points'] = st.number_input("Points")
-
-    st.session_state['draft']['assignment_type'] = st.selectbox("Type", ["Select an option", "Homework", "Lab", "other"])
-    if st.session_state["draft"]['assignment_type'] == "other":
-        st.session_state["draft"]['assignment_type'] = st.text_input("Type", placeholder="Enter the assignment Type")
-
-    # Live Preview
     with st.container(border=True):
-        with st.expander("Assignment Details", expanded=True):
-            # Using .get() for safe dictionary access on initial load
-            st.markdown(f"### Title: {st.session_state['draft'].get('title', '')}")
-            st.markdown(f"**Description**: {st.session_state['draft'].get('description', '')}")
-            st.markdown(f"Type: **{st.session_state['draft'].get('assignment_type', '')}**")
+        for assignment in assignments:
+            with st.container(border = True):
+                st.markdown(f"**Title:** {assignment['title']}")
+                st.markdown(f"**Description:** {assignment['description']}")
+                if st.button("Edit", key=f"edit_btn_{assignment['id']}", type="primary",use_container_width=True):
+                    st.session_state["page"] = "Edit Assignment"
+                    st.session_state["draft"] = assignment
+
+                    st.rerun()
+
+def render_add_new_assignment(assignments, json_path):
+    col1, col2 = st.columns([3, 1])
+    with col1:
+        if st.session_state["page"] == "Add New Assignment":
+            st.subheader("Add New Assignment")
+        elif st.session_state["page"] == "Edit Assignment":
+            st.subheader("Edit Assignment")
+    with col2:
+        if st.button("Back", key="back_btn", type="secondary"):
+            st.session_state["page"] = "Assignments Dashboard" 
+            st.rerun()
+    # Form Inputs
+        st.session_state['draft']['title'] = st.text_input("Title", key="title_txt_input", value=st.session_state['draft']['title'])
+        st.session_state['draft']['description'] = st.text_area("Description", key="description_txt_area", value=st.session_state['draft']['description'], placeholder="normalization is covered here",
+                                help="Here you are entering the assignment details")
+        st.session_state['draft']['points'] = st.number_input("Points", key="points_num_input", value=st.session_state["draft"]['points'])
+
+        st.session_state['draft']['assignment_type'] = st.selectbox("Type", ["Select an option", "Homework", "Lab", "other"])
+        if st.session_state['draft']['assignment_type'] == "other":
+            st.session_state['draft']['assignment_type'] = st.text_input("Type", placeholder="Enter the assignment Type")
+
     
     # Save Action
-    btn_save = st.button("Save", use_container_width=True, key="save_btn", type="primary")
+    if st.button("Save", use_container_width=True, key="save_btn", type="primary"):
+        with st.spinner("Assignment is being recorded..."):
+            time.sleep(3)
 
-    if btn_save:
-        if not st.session_state['draft'].get('title'):
-            st.warning("Title needs to be provided!")
-        else:
-            with st.spinner("Assignment is being recorded...."):
-                time.sleep(2)
-                
-                # Append to list
-                assignments.append(
-                    {
-                        "id": str(uuid.uuid4()),
-                        "title": st.session_state['draft']['title'],
-                        "description": st.session_state['draft']['description'],
-                        "points": st.session_state['draft']['points'],
-                        "type": st.session_state['draft']['assignment_type']
-                    }
-                )
+        #add new assigbment 
+        if st.session_state["page"] == "Add New Assignment":
+            assignments = add_new_assignment(assignments)
 
-                # Record directly into JSON file 
-                with open(json_path, "w", encoding="utf-8") as f:
-                    json.dump(assignments, f, indent=4)
+        elif st.session_state["page"] == "Edit Assignment":
+            assignments = edit_assignment(assignments)
 
-                st.success("New Assignment is recorded!")
-                st.info("This is a new assignment")
-                
-                time.sleep(2)
-                st.session_state["page"] = "Assignments Dashboard"
-                st.session_state["draft"] = {}
-                st.rerun()
-def render_add_new_assignment(assignments, json_path):
-    pass
+        #record data into json file
+        save_data(assignments, json_path)
+        st.success("New Assignment is recorded!")
+        time.sleep(3)
+        st.session_state["draft"] = {}
+        st.session_state["page"] = "Assignments Dashboard"
+        st.rerun()  
+        
 
 def main():
-    st.set_page_config(page_title="Assignment Management", page_icon=":mortar_board:", layout="collapsed")
+    st.set_page_config(page_title="Assignment Management", page_icon=":mortar_board:", layout="collapsed", initial_sidebar_state="collapsed")
     st.title("Assignment Management Application")
     #loading the daav 
     assignments = []
-
     json_path = Path("assignments.json")
+    assignments = load_data(json_path)
+
     #3 session srate initialization 
     if "page" not in st.session_state:
         st.session_state["page"] = "Assignments Dashboard"
@@ -113,6 +122,7 @@ def main():
     elif st.session_state["page"] == "Add New Assignment":
         render_add_new_assignment(assignments=assignments, json_path=json_path)
     elif st.session_state["page"] == "Edit Assignment":
+        render_add_new_assignment(assignments=assignments, json_path=json_path)
         st.rerun()
         pass
 
